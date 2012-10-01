e968cb8d22fa0d1987c6dbe5537aecd90d7890e7
Isabel Restrepo <isabelrestre@gmail.com>
2012-10-01 10:29:27 -0400

    use newer ICP pipeline

--- pybindings/processes/vpcl_register_icp_process.cxx
+++ pybindings/processes/vpcl_register_icp_process.cxx
@@ -25,7 +25,7 @@
 //:global variables
 namespace vpcl_register_icp_process_globals 
 {
-  const unsigned n_inputs_ = 9;
+  const unsigned n_inputs_ = 11;
   const unsigned n_outputs_ = 0;
 }
 
@@ -44,8 +44,12 @@ bool vpcl_register_icp_process_cons(bprb_func_process& pro)
   input_types_[i++] = "double";
   input_types_[i++] = "double";
   input_types_[i++] = "double";
-  input_types_[i++] = "double";
   input_types_[i++] = "int";
+  input_types_[i++] = "double";
+  input_types_[i++] = "bool";
+  input_types_[i++] = "bool";
+
+
   
   vcl_vector<vcl_string> output_types_(n_outputs_);
   
@@ -67,37 +71,47 @@ bool vpcl_register_icp_process(bprb_func_process& pro)
   vcl_string tform_cloud_fname = pro.get_input<vcl_string>(i++);
   vcl_string tform_fname = pro.get_input<vcl_string>(i++);
   double max_correspondence_dist = pro.get_input<double>(i++);
-  double outlier_rejection_threshold = pro.get_input<double>(i++);
-  double transformation_epsilon = pro.get_input<double>(i++);
-  double euclidean_epsilon = pro.get_input<double>(i++);
+  double translation_epsilon = pro.get_input<double>(i++);
+  double rotation_epsilon = pro.get_input<double>(i++);
   int max_iterations = pro.get_input<int>(i++);
+  double max_normal_angle = pro.get_input<double>(i++);
+  bool reject_normals = pro.get_input<bool>(i++);
+  bool use_lm = pro.get_input<bool>(i++);
+
   
   cout <<  "\nSource: " << src_fname << "\nTarget : " << tgt_fname
-  <<"\neuclidean_epsilon: " << transformation_epsilon
-   <<"\ntransformation_epsilon: " << transformation_epsilon
   << "\nmax_correspondence_dist:" << max_correspondence_dist
-  <<"\noutlier_rejection_threshold: " << outlier_rejection_threshold
+  <<"\ntranslation_epsilon: " << translation_epsilon
+  <<"\nrotation_epsilon: " << rotation_epsilon
+  <<"\nmax_normal_angle: " << max_normal_angle
+  <<"\nreject_normals: " << reject_normals
+  <<"\nuse_lm: " << use_lm
   << "\nmax_iterations: " <<max_iterations <<endl ;
   
   //Load input point clouds
   PointCloud<PointNormal>::Ptr src_points(new PointCloud<PointNormal>);
-  vpcl_io_util::load_cloud(src_fname, src_points);
+  vpcl_io_util::load_cloud<PointNormal>(src_fname, src_points);
   PointCloud<PointNormal>::Ptr tgt_points(new PointCloud<PointNormal>);
-  vpcl_io_util::load_cloud(tgt_fname, tgt_points);
+  vpcl_io_util::load_cloud<PointNormal>(tgt_fname, tgt_points);
 
   bool converged = false;
   double score = -1.0;
-  Eigen::Matrix4f tform;
+  Eigen::Matrix4d tform;
   tform.setIdentity ();
   console::TicToc tt;
   tt.tic();
-  tform = vpcl::refineAlignment<PointNormal,PointNormal> (src_points, tgt_points, tform, max_correspondence_dist,
-                                                          outlier_rejection_threshold, transformation_epsilon, euclidean_epsilon, max_iterations, converged, score);
+  
+//  tform = vpcl::refineAlignment<PointNormal,PointNormal> (src_points, tgt_points, tform, max_correspondence_dist,
+//                                                          outlier_rejection_threshold, transformation_epsilon, euclidean_epsilon, max_iterations, converged, score);
+  
+  int iterations = vpcl::icp_with_rejection<PointNormal>(src_points, tgt_points, tform, max_iterations, translation_epsilon, rotation_epsilon, max_correspondence_dist, true,  max_normal_angle, reject_normals, use_lm);
+  
   double trans_time = tt.toc();
   pcl::console::print_info ("Refined alignment\n");
   
   // Transform the source point to align them with the target points
-  pcl::transformPointCloud (*src_points, *src_points, tform);
+  Eigen::Matrix4f tform_f = tform.cast<float>();
+  pcl::transformPointCloud (*src_points, *src_points, tform_f);
   
   // Save output
   pcl::io::savePCDFile (tform_cloud_fname, *src_points);
@@ -112,7 +126,7 @@ bool vpcl_register_icp_process(bprb_func_process& pro)
   
   ofstream time_ofs;
   time_ofs.open(time_fname.c_str());
-  time_ofs << "Trans Time: " << trans_time;
+  time_ofs << "Trans Time: " << trans_time << "\nNo. Iter: " << iterations;
   ofs.close();
   
   return true;
